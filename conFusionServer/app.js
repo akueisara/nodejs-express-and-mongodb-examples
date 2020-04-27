03fa8beaaeefd587bb31b7fc10cdc35bbf9c1470
Kuei-Jung Hu <akueisara39@gmail.com>
2020-04-27 15:21:23 +0900

    Cookies

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -28,25 +28,37 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser('12345-67890-09876-54321'));
 
 function auth(req, res, next) {
-  console.log(req.headers);
+  console.log(req.signedCookies);
 
-  var authHeader = req.headers.authorization;
+  if (!req.signedCookies.user) {
+    var authHeader = req.headers.authorization;
 
-  if (!authHeader) {
-    notAuthenticated(res, next);
-  }
+    if (!authHeader) {
+      notAuthenticated(res, next);
+    }
 
-  var auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':');
-  var username = auth[0];
-  var password = auth[1];
+    var auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':');
+    var username = auth[0];
+    var password = auth[1];
 
-  if (username === 'admin' && password === 'password') {
-    next();
+    if (username === 'admin' && password === 'password') {
+      res.cookie('user', 'admin', { signed: true })
+      next();
+    } else {
+      notAuthenticated(res, next);
+    }
   } else {
-    notAuthenticated(res, next);
+    if (req.signedCookies.user === 'admin') {
+      next();
+    } else {
+      var err = new Error('You are not authenticated!');
+
+      err.status = 401;
+      return next(err);
+    }
   }
 }
 
