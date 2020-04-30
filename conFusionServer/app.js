3049691da2a05c7c7bb9fac9b073b73bf21f16e6
Kuei-Jung Hu <akueisara39@gmail.com>
2020-04-30 15:48:13 +0900

    HTTPS

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -28,6 +28,15 @@ connect.then((db) => {
 
 var app = express();
 
+// Secure traffic only
+app.all('*', (req, res, next) => {
+  if (req.secure) {
+    return next();
+  } else {
+    res.redirect(307, 'http://' + req.hostname + ':' + app.get('secPort') + req.url);
+  }
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
