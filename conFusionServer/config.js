f0490ba19cb6a673ce70eec2a22a36852c0b3f8e
Kuei-Jung Hu <akueisara39@gmail.com>
2020-05-02 23:44:53 +0900

    Passport Facebook

--- conFusionServer/config.js
+++ conFusionServer/config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '238147620588526',
+        clientSecret: 'Your Client App Secret'
+    }
 }
