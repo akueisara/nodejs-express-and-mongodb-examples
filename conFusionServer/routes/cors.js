6914554f02170b7974b8b37cb7dbcde85969d6cb
Kuei-Jung Hu <akueisara39@gmail.com>
2020-05-04 23:15:21 +0900

    Add React client app

--- conFusionServer/routes/cors.js
+++ conFusionServer/routes/cors.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-var whitelist = ['http://localhost:3000', 'https://localhost:3443', 'http://kueijunghu.com:3001'];
+var whitelist = ['http://localhost:3000', 'https://localhost:3443', 'http://localhost:3001'];
 var corsOptionsDelegate = (req, callback) => {
   var corsOptions;
   console.log(req.header('Origin'));
